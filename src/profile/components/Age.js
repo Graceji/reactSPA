35e9e70bb869dbb8011e1d0c637637ba07e2515f
Grace_ji <jina@mufengcm.com>
2017-02-16 11:25:00 +0800

    完成导航一

--- src/profile/components/Age.js
+++ src/profile/components/Age.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import { DatePicker, Input } from 'antd';
+
+const { MonthPicker, RangePicker } = DatePicker;
 
 
 class Age extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trueAge: 0
+    };
+    
+    this.onChange = this.onChange.bind(this);
+    
+  }
+
+  onChange(date) {
+    // let now = (new Date()).toDateString();
+    let msGap = new Date().getTime() - new Date(date.toDate()).getTime();
+    let trueAge = (msGap/1000/3600/24/365.242199).toFixed(2);
+    this.setState({
+      trueAge
+    });
+  }
+
   render() {
     return (
       <div>
-        您的出生日期：
-        
+        <div>
+          您的出生日期：
+          <DatePicker onChange={this.onChange} />
+        </div>
+        <div style={{ marginBottom: 16 }}>
+          <Input
+            addonBefore="您已在地球上存活了："
+            addonAfter="年"
+            value={this.state.trueAge}
+          />
+        </div>
       </div>
     );
   }
